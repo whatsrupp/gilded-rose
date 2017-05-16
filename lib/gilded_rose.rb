9c4ea398bf3f87ed3a8dd8105fb83775e71689e9
Nick Rupp <nickrupp95@gmail.com>
2017-05-16 12:10:03 +0100

    Extract all conditionals into individual methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,29 +4,53 @@ class GildedRose
     @items = items
   end
 
+  def isNotAppreciatorItem?(item)
+    item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def isNotLegendary?(item)
+    item.name != "Sulfuras, Hand of Ragnaros"
+  end
+
+  def isNotBrie?(item)
+    item.name != "Aged Brie"
+  end
+
+  def isNotBackstagePass?(item)
+    item.name != "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def hasValue?(item)
+    item.quality > 0
+  end
+
+  def isNotMaxValue?(item)
+    item.quality < 50
+  end
+
   def update_quality()
     @items.each do |item|
       # THIS WHOLE BLOCK IS QUALITY DROP
       # Regular Item Depreciation
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
+      if isNotAppreciatorItem?(item)
+        if hasValue?(item)
+          if isNotLegendary?(item)
             item.quality = item.quality - 1
           end
         end
       else
         # Item Appreciates if it's an appreciator
-        if item.quality < 50
+        if isNotMaxValue?(item)
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if !isNotBackstagePass?(item)
             # Apprecitate Back Stage Pass
             if item.sell_in < 11
-              if item.quality < 50
+              if isNotMaxValue?(item)
                 item.quality = item.quality + 1
               end
             end
             if item.sell_in < 6
-              if item.quality < 50
+              if isNotMaxValue?(item)
                 item.quality = item.quality + 1
               end
             end
@@ -34,23 +58,21 @@ class GildedRose
         end
       end
       # UPDATE SELL IN
-      if item.name != "Sulfuras, Hand of Ragnaros"
+      if isNotLegendary?(item)
         item.sell_in = item.sell_in - 1
       end
       # Past sell by date
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
+        if isNotBrie?(item)
+          if isNotBackstagePass?(item)
+            if hasValue?(item) and isNotLegendary?(item)
+              item.quality = item.quality - 1
             end
           else
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
+          if isNotMaxValue?(item)
             item.quality = item.quality + 1
           end
         end
