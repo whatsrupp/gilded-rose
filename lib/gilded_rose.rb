a82064c0d280b07d0d923627344958a46ed8aa0b
Nick Rupp <nickrupp95@gmail.com>
2017-05-16 15:38:58 +0100

    upate all variable names to snake case, turns out I was still in JS mode

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,7 +4,15 @@ class GildedRose
     @items = items
   end
 
-  def isNotAppreciatorItem?(item)
+  def update_quality()
+    @items.each do |item|
+      regular_quality_update(item)
+      update_sell_in(item)
+      extra_quality_updates(item)
+    end
+  end
+
+  def is_not_appreciator_item?(item)
     item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
   end
 
@@ -12,12 +20,16 @@ class GildedRose
     item.name != "Sulfuras, Hand of Ragnaros"
   end
 
-  def isNotBrie?(item)
+  def is_not_brie?(item)
     item.name != "Aged Brie"
   end
 
-  def isNotBackstagePass?(item)
+  def is_not_backstage_pass?(item)
     item.name != "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def is_backstage_pass?(item)
+    item.name == "Backstage passes to a TAFKAL80ETC concert"
   end
 
   def hasValue?(item)
@@ -28,55 +40,65 @@ class GildedRose
     item.quality < 50
   end
 
-  def update_quality()
-    @items.each do |item|
-      # THIS WHOLE BLOCK IS QUALITY DROP
-      # Regular Item Depreciation
-      if isNotAppreciatorItem?(item)
-        if hasValue?(item)
-          if isNotLegendary?(item)
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        # Item Appreciates if it's an appreciator
-        if isNotMaxValue?(item)
-          item.quality = item.quality + 1
-          if !isNotBackstagePass?(item)
-            # Apprecitate Back Stage Pass
-            if item.sell_in < 11
-              if isNotMaxValue?(item)
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if isNotMaxValue?(item)
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      # UPDATE SELL IN
+  def isDepreciatorItemWithValue?(item)
+    is_not_appreciator_item?(item) && hasValue?(item) && isNotLegendary?(item)
+  end
+
+  def depreciate_item_value(item)
+    if hasValue?(item)
       if isNotLegendary?(item)
-        item.sell_in = item.sell_in - 1
-      end
-      # Past sell by date
-      if item.sell_in < 0
-        if isNotBrie?(item)
-          if isNotBackstagePass?(item)
-            if hasValue?(item) and isNotLegendary?(item)
-              item.quality = item.quality - 1
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if isNotMaxValue?(item)
-            item.quality = item.quality + 1
-          end
-        end
+        item.quality = item.quality - 1
       end
     end
   end
+
+  def appreciate_more_if_backstage_pass(item)
+    # Apprecitate Back Stage Pass
+    if is_backstage_pass?(item)
+      if item.sell_in < 11
+        appreciate_item_value(item)
+      end
+      if item.sell_in < 6
+        appreciate_item_value(item)
+      end
+    end
+  end
+
+  def appreciate_item_value(item)
+    if isNotMaxValue?(item)
+      item.quality = item.quality + 1
+    end
+  end
+
+  def update_sell_in(item)
+    if isNotLegendary?(item)
+      item.sell_in = item.sell_in - 1
+    end
+  end
+
+  def regular_quality_update(item)
+    if is_not_appreciator_item?(item)
+      depreciate_item_value(item)
+    else
+      appreciate_item_value(item)
+      appreciate_more_if_backstage_pass(item)
+    end
+  end
+
+  def extra_quality_updates(item)
+    if item.sell_in < 0
+      if is_not_brie?(item)
+        if is_not_backstage_pass?(item)
+          if hasValue?(item) and isNotLegendary?(item)
+            item.quality = item.quality - 1
+          end
+        else
+          item.quality = item.quality - item.quality
+        end
+      else
+        appreciate_item_value(item)
+      end
+    end
+  end
+
 end
