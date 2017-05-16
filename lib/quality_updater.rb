bf974ca828e505e48581c80efc5dd69e050acd3b
Nick Rupp <nickrupp95@gmail.com>
2017-05-16 16:11:02 +0100

    add set_item method to quality updater

--- lib/quality_updater.rb
+++ lib/quality_updater.rb
@@ -1,3 +1,15 @@
 class QualityUpdater
+  public
+  attr_reader :item
 
+  def initializer()
+    @item  = nil
+  end
+
+  def set_item(new_item)
+    self.item = new_item
+  end
+  
+  private
+  attr_writer :item
 end
