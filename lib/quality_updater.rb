9a6e6ab37ca2a77dde7bf3f05253603704d871d6
Nick Rupp <nickrupp95@gmail.com>
2017-05-16 17:29:30 +0100

    finish quality_updater_functionality, prepared for extraction from gilded_rose

--- lib/quality_updater.rb
+++ lib/quality_updater.rb
@@ -9,7 +9,99 @@ class QualityUpdater
   def set_item(new_item)
     self.item = new_item
   end
-  
+
+  def update
+    perform_initial_quality_updates
+    update_sell_in
+    perform_extra_quality_updates
+  end
+
+  def is_not_appreciator_item?
+    item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def is_not_legendary?
+    item.name != "Sulfuras, Hand of Ragnaros"
+  end
+
+  def is_not_brie?
+    item.name != "Aged Brie"
+  end
+
+  def is_not_backstage_pass?
+    item.name != "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def is_backstage_pass?
+    item.name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def has_value?
+    item.quality > 0
+  end
+
+  def is_not_max_value?
+    item.quality < 50
+  end
+
+  def is_depreciator_item_with_value?
+    is_not_appreciator_item? && has_value? && is_not_legendary?
+  end
+
+  def depreciate_item_value
+    if has_value?
+      if is_not_legendary?
+        item.quality = item.quality - 1
+      end
+    end
+  end
+
+  def appreciate_more_if_backstage_pass
+    if is_backstage_pass?
+      if item.sell_in < 11
+        appreciate_item_value
+      end
+      if item.sell_in < 6
+        appreciate_item_value
+      end
+    end
+  end
+
+  def appreciate_item_value
+    if is_not_max_value?
+      item.quality = item.quality + 1
+    end
+  end
+
+  def update_sell_in
+    if is_not_legendary?
+      item.sell_in = item.sell_in - 1
+    end
+  end
+
+  def perform_initial_quality_updates
+    if is_not_appreciator_item?
+      depreciate_item_value
+    else
+      appreciate_item_value
+      appreciate_more_if_backstage_pass
+    end
+  end
+
+  def perform_extra_quality_updates
+    if item.sell_in < 0
+      if is_not_brie?
+        if is_not_backstage_pass?
+          depreciate_item_value
+        else
+          item.quality = item.quality - item.quality
+        end
+      else
+        appreciate_item_value
+      end
+    end
+  end
+
   private
   attr_writer :item
 end
--- spec/quality_updater_spec.rb
+++ spec/quality_updater_spec.rb
@@ -9,169 +9,171 @@ describe QualityUpdater do
       expect(quality_updater.item).to eq(item)
     end
   end
-  #
-  # describe 'Item Type:' do
-  #   describe 'Legendary' do
-  #     describe 'Sulfuras' do
-  #       it 'does not drop in quality' do
-  #         items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
-  #         GildedRose.new(items).update_quality()
-  #         expect(items[0].quality).to eq 80
-  #       end
-  #     end
-  #   end
-  #
-  #   describe 'Depreciator' do
-  #     context 'within sell by date' do
-  #       it 'decreases by 1' do
-  #         items = [Item.new(name= 'Chboys Chips',
-  #                  sell_in= 20,
-  #                  quality= 20)]
-  #         GildedRose.new(items).update_quality()
-  #         expect(items[0].quality).to eq 19
-  #         expect(items[0].sell_in).to eq 19
-  #
-  #       end
-  #       it 'but not past 50' do
-  #         items = [Item.new(name= 'Chboys Chips',
-  #                  sell_in= 20,
-  #                  quality= 0)]
-  #         GildedRose.new(items).update_quality()
-  #         expect(items[0].quality).to eq 0
-  #         expect(items[0].sell_in).to eq 19
-  #
-  #       end
-  #     end
-  #     context 'after sell by date' do
-  #       it 'decreases by 2' do
-  #         items = [Item.new(name= 'Chboys Chips',
-  #                  sell_in= 0,
-  #                  quality= 20)]
-  #         GildedRose.new(items).update_quality()
-  #         expect(items[0].quality).to eq 18
-  #         expect(items[0].sell_in).to eq -1
-  #       end
-  #       it 'but not past 50' do
-  #         items = [Item.new(name= 'Chboys Chips',
-  #                  sell_in= 0,
-  #                  quality= 0)]
-  #         GildedRose.new(items).update_quality()
-  #         expect(items[0].quality).to eq 0
-  #         expect(items[0].sell_in).to eq -1
-  #
-  #       end
-  #     end
-  #   end
-  #
-  #   describe 'Appreciator' do
-  #     describe 'Backstage Passes' do
-  #       context 'before concert' do
-  #         context '11 days or more to go' do
-  #           it 'quality increases by 1' do
-  #             items = [Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
-  #                      sell_in= 11,
-  #                      quality= 20)]
-  #             GildedRose.new(items).update_quality()
-  #             expect(items[0].quality).to eq 21
-  #             expect(items[0].sell_in).to eq 10
-  #
-  #           end
-  #
-  #           it 'but not past 50' do
-  #             items = [Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
-  #                      sell_in= 11,
-  #                      quality= 50)]
-  #             GildedRose.new(items).update_quality()
-  #             expect(items[0].quality).to eq 50
-  #             expect(items[0].sell_in).to eq 10
-  #
-  #           end
-  #         end
-  #         context '6-10 days days to go' do
-  #           it 'quality increases by 2' do
-  #             items = [Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
-  #                      sell_in= 6,
-  #                      quality= 20)]
-  #             GildedRose.new(items).update_quality()
-  #             expect(items[0].quality).to eq 22
-  #             expect(items[0].sell_in).to eq 5
-  #
-  #           end
-  #           it 'but not past 50' do
-  #             items = [Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
-  #                      sell_in= 6,
-  #                      quality= 49)]
-  #             GildedRose.new(items).update_quality()
-  #             expect(items[0].quality).to eq 50
-  #             expect(items[0].sell_in).to eq 5
-  #
-  #           end
-  #         end
-  #         context '5 days left' do
-  #           it 'quality increases by 3' do
-  #             items = [Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
-  #                      sell_in= 5,
-  #                      quality= 20)]
-  #             GildedRose.new(items).update_quality()
-  #             expect(items[0].quality).to eq 23
-  #             expect(items[0].sell_in).to eq 4
-  #
-  #           end
-  #           it 'but not past 50' do
-  #             items = [Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
-  #                      sell_in= 5,
-  #                      quality= 48)]
-  #             GildedRose.new(items).update_quality()
-  #             expect(items[0].quality).to eq 50
-  #             expect(items[0].sell_in).to eq 4
-  #
-  #           end
-  #         end
-  #       end
-  #       context 'after concert' do
-  #         it 'drop to 0 quality' do
-  #           items = [Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
-  #                    sell_in= 0,
-  #                    quality= 20)]
-  #           GildedRose.new(items).update_quality()
-  #           expect(items[0].quality).to eq 0
-  #           expect(items[0].sell_in).to eq -1
-  #
-  #         end
-  #       end
-  #     end
-  #
-  #     describe 'Finely Aged Brie' do
-  #       it 'increases in quality' do
-  #         items = [Item.new(name= "Aged Brie",
-  #                  sell_in= 5,
-  #                  quality= 20)]
-  #         GildedRose.new(items).update_quality()
-  #         expect(items[0].quality).to eq 21
-  #         expect(items[0].sell_in).to eq 4
-  #
-  #       end
-  #       it 'but not past 50' do
-  #         items = [Item.new(name= "Aged Brie",
-  #                  sell_in= 5,
-  #                  quality= 50)]
-  #         GildedRose.new(items).update_quality()
-  #         expect(items[0].quality).to eq 50
-  #         expect(items[0].sell_in).to eq 4
-  #
-  #       end
-  #     end
-  #   end
-  #
-  #   describe 'Conjured' do
-  #     xit 'drops by 2 quality' do
-  #       items = [Item.new(name= 'Conjured Chboys Chips',
-  #                sell_in= 20,
-  #                quality= 20)]
-  #       GildedRose.new(items).update_quality()
-  #       expect(items[0].quality).to eq 18
-  #       expect(items[0].sell_in).to eq 19
-  #     end
-  #   end
-  # end
+
+  describe 'Item Type:' do
+    describe 'Legendary' do
+      describe 'Sulfuras' do
+        it 'does not drop in quality' do
+          item = Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+          quality_updater.set_item(item)
+          quality_updater.update
+          expect(item.quality).to eq 80
+        end
+      end
+    end
+
+    describe 'Depreciator' do
+      context 'within sell by date' do
+        it 'decreases by 1' do
+          item = Item.new(name= 'Chboys Chips',
+                   sell_in= 20,
+                   quality= 20)
+          quality_updater.set_item(item)
+          quality_updater.set_item(item)
+          quality_updater.update
+          expect(item.quality).to eq 19
+
+        end
+        it 'but not past 50' do
+          item = Item.new(name= 'Chboys Chips',
+                   sell_in= 20,
+                   quality= 0)
+          quality_updater.set_item(item)
+          quality_updater.update
+          expect(item.quality).to eq 0
+
+        end
+      end
+      context 'after sell by date' do
+        it 'decreases by 2' do
+          item = Item.new(name= 'Chboys Chips',
+                   sell_in= 0,
+                   quality= 20)
+          quality_updater.set_item(item)
+          quality_updater.update
+          expect(item.quality).to eq 18
+        end
+        it 'but not past 50' do
+          item = Item.new(name= 'Chboys Chips',
+                   sell_in= 0,
+                   quality= 0)
+          quality_updater.set_item(item)
+          quality_updater.update
+          expect(item.quality).to eq 0
+
+        end
+      end
+    end
+
+    describe 'Appreciator' do
+      describe 'Backstage Passes' do
+        context 'before concert' do
+          context '11 days or more to go' do
+            it 'quality increases by 1' do
+              item = Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
+                       sell_in= 11,
+                       quality= 20)
+              quality_updater.set_item(item)
+              quality_updater.update
+              expect(item.quality).to eq 21
+
+            end
+
+            it 'but not past 50' do
+              item = Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
+                       sell_in= 11,
+                       quality= 50)
+              quality_updater.set_item(item)
+              quality_updater.update
+              expect(item.quality).to eq 50
+
+            end
+          end
+          context '6-10 days days to go' do
+            it 'quality increases by 2' do
+              item = Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
+                       sell_in= 6,
+                       quality= 20)
+              quality_updater.set_item(item)
+              quality_updater.update
+              expect(item.quality).to eq 22
+
+            end
+            it 'but not past 50' do
+              item = Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
+                       sell_in= 6,
+                       quality= 49)
+              quality_updater.set_item(item)
+              quality_updater.update
+              expect(item.quality).to eq 50
+
+            end
+          end
+          context '5 days left' do
+            it 'quality increases by 3' do
+              item = Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
+                       sell_in= 5,
+                       quality= 20)
+              quality_updater.set_item(item)
+              quality_updater.update
+              expect(item.quality).to eq 23
+
+            end
+            it 'but not past 50' do
+              item = Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
+                       sell_in= 5,
+                       quality= 48)
+              quality_updater.set_item(item)
+              quality_updater.update
+              expect(item.quality).to eq 50
+
+            end
+          end
+        end
+        context 'after concert' do
+          it 'drop to 0 quality' do
+            item = Item.new(name= "Backstage passes to a TAFKAL80ETC concert",
+                     sell_in= 0,
+                     quality= 20)
+            quality_updater.set_item(item)
+            quality_updater.update
+            expect(item.quality).to eq 0
+
+          end
+        end
+      end
+
+      describe 'Finely Aged Brie' do
+        it 'increases in quality' do
+          item = Item.new(name= "Aged Brie",
+                   sell_in= 5,
+                   quality= 20)
+          quality_updater.set_item(item)
+          quality_updater.update
+          expect(item.quality).to eq 21
+
+        end
+        it 'but not past 50' do
+          item = Item.new(name= "Aged Brie",
+                   sell_in= 5,
+                   quality= 50)
+          quality_updater.set_item(item)
+          quality_updater.update
+          expect(item.quality).to eq 50
+
+        end
+      end
+    end
+
+    describe 'Conjured' do
+      xit 'drops by 2 quality' do
+        item = Item.new(name= 'Conjured Chboys Chips',
+                 sell_in= 20,
+                 quality= 20)
+        quality_updater.set_item(item)
+        quality_updater.update
+        expect(item.quality).to eq 18
+      end
+    end
+  end
 end
